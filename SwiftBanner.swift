4f53dbfc6538f0d67442b2a16bf9c8a230f856df
HongPu <hongpu@HongPudeMacBook-Pro.local>
2015-12-17 17:57:49 +0800

    first commit

---
+++ SwiftBanner.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftBanner.swift
+//  SwiftBanner
+//
+//  Created by HongPu on 2015/12/17.
+//  Copyright © 2015年 tinyxx. All rights reserved.
+//
+
+import UIKit
+
+class SwiftBanner: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SwiftBanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBanner
+//
+//  Created by HongPu on 2015/12/17.
+//  Copyright © 2015年 tinyxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
