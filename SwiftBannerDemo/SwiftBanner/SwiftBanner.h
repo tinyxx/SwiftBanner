1c0a763f391139d3eb182789b1c78d80c8472149
tinyxx <tinyxx415@gmail.com>
2015-12-28 14:09:52 +0800

    support carthage

---
+++ SwiftBannerDemo/SwiftBanner/SwiftBanner.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBanner.h
+//  SwiftBanner
+//
+//  Created by HongPu on 2015/12/28.
+//  Copyright © 2015年 tinyxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftBanner.
+FOUNDATION_EXPORT double SwiftBannerVersionNumber;
+
+//! Project version string for SwiftBanner.
+FOUNDATION_EXPORT const unsigned char SwiftBannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBanner/PublicHeader.h>
+
+
